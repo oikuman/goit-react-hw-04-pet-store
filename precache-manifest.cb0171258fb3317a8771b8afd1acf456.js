475be85f836a02e258c46f8cba84b886817a9109
oikuman <oikuma@gmail.com>
2019-10-21 20:47:42 +0300

    Updates

---
+++ precache-manifest.cb0171258fb3317a8771b8afd1acf456.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e841428178714216b32b1bcaac7c53c",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "210c5b1378a199893da3",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.859de781.chunk.css"
+  },
+  {
+    "revision": "61f4d9bd3bcf63aa888f",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.2947ce08.chunk.js"
+  },
+  {
+    "revision": "210c5b1378a199893da3",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.57a01238.chunk.js"
+  },
+  {
+    "revision": "07e98775093de33e0f58",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.60205102.js"
+  }
+]);
